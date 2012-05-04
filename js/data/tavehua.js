820d734d2a2056567cc008f54342f3c48c6b2bc8
Georgia Bullen <georgia.bullen@gmail.com>
2012-05-04 17:35:56 -0400

    map adjustments for example project

--- js/data/tavehua.js
+++ js/data/tavehua.js
@@ -9,15 +9,15 @@
 			"media_geo_longitude": -96.26240,
 			"child_items_count": 4,
 			"attributes": {
-				"tiles":"zeega.TavehuaRoads"
+				"tiles":"zeega.map-xnf3kyxb"
 			},
 			"child_items": [
 				{
 					"id": 164,
 					"title": "Rituals in Tavehua",
 					"description": "",
-					"media_geo_latitude": 17.24077,
-					"media_geo_longitude": -96.26240,
+					"media_geo_latitude": 17.24036,
+					"media_geo_longitude": -96.26180,
 					"attributes": {
 							"project_id":257,
 							"frame_id":3187
@@ -27,8 +27,8 @@
 					"id": 165,
 					"title": "Concrete Walls: Change in Tavehua",
 					"description": "",
-					"media_geo_latitude": 17.23980,
-					"media_geo_longitude": -96.26165,
+					"media_geo_latitude": 17.23970,
+					"media_geo_longitude": -96.26088,
 					"attributes": {
 							"project_id":257,
 							"frame_id":3197
