4ce416288bff817052799f7f326f527ff5e09326
Georgia Bullen <georgia.bullen@gmail.com>
2012-05-08 12:00:30 -0400

    Limuru Project - Jihoi & Benedetta

---
+++ js/data/limuru.js
@@ -0,0 +1,43 @@
+{
+	"items": [
+		{
+			"id": 190,
+			"title": "Limuru, Kenya",
+			"description": "",
+			"text": "",
+			"media_geo_latitude": -1.16920170, 
+			"media_geo_longitude": 36.69032890,
+			"child_items_count": 2,
+			"attributes": {
+				"tiles":"zeega.Limuru"
+			},
+			"child_items": [
+				{
+					"id": 191,
+					"title": "Paterson, NJ ---- Limuru, Kenya",
+					"description": "",
+					"media_geo_latitude": -1.1519,
+					"media_geo_longitude": 36.6701,
+					"attributes": {
+							"project_id":833,
+							"frame_id":4456
+					}
+				},
+				{
+					"id": 165,
+					"title": "Limuru, Kenya",
+					"description": "",
+					"media_geo_latitude": -1.16920170,
+					"media_geo_longitude": 36.69032890,
+					"attributes": {
+							"project_id":707,
+							"frame_id":3509
+						}
+				}
+			],
+			"tags": []
+		}
+	],
+	
+	"request": {}
+}
---
+++ js/data/patterson.js
@@ -0,0 +1,54 @@
+{
+	"items": [
+		{
+			"id": 190,
+			"title": "Paterson, NJ",
+			"description": "",
+			"text": "",
+			"media_geo_latitude": 40.91676540,
+			"media_geo_longitude": -74.17181100,
+			"child_items_count": 4,
+			"attributes": {
+				"tiles":"zeega.Paterson"
+			},
+			"child_items": [
+				{
+					"id": 191,
+					"title": "Paterson, NJ ---- Limuru, Kenya",
+					"description": "",
+					"media_geo_latitude": 40.91676540,
+					"media_geo_longitude": -74.17181100,
+					"attributes": {
+							"project_id":833,
+							"frame_id":4456
+					}
+				},
+				{
+					"id": 165,
+					"title": "Paterson, NJ",
+					"description": "",
+					"media_geo_latitude": 40.91914,
+					"media_geo_longitude": -74.17814,
+					"attributes": {
+							"project_id":687,
+							"frame_id":3444
+						}
+				},
+				{
+					"id": 166,
+					"title": "Uhuru Garden",
+					"description": "",
+					"media_geo_latitude": 40.9161,
+					"media_geo_longitude": -74.1928,
+					"attributes": {
+							"project_id":683,
+							"frame_id":3423
+						}
+				}
+			],
+			"tags": []
+		}
+	],
+	
+	"request": {}
+}
