acec6a638d4302c71dae3571e6e7f628ca95343a
Georgia Bullen <georgia.bullen@gmail.com>
2012-05-08 17:23:51 -0400

    heejin & eduardo projects

---
+++ js/data/nyc-korean-edu.js
@@ -0,0 +1,43 @@
+{
+	"items": [
+		{
+			"id": 182,
+			"title": "Korilla BBQ Truck",
+			"description": "The project attempts to explore the personal remittance stories of immigrant workers involved in the food truck business. We take three cases – two halal trucks and a Korilla BBQ truck - all located on Amsterdam Avenue with 116th street, Columbia University.   To frame the personal stories and the conditions of transnational economic frameworks we focus on the efficiency of the trucks as well as the business models that support them. The project interweaves personal interviews as well as found audiovisual material of the internet.",
+			"text": "",
+			"media_geo_latitude": 40.80832, 
+			"media_geo_longitude": -73.96177,
+			"child_items_count": 4,
+			"attributes": {
+				"tiles":"zeega.KorillaBBQTrucks"
+			},
+			"child_items": [
+				{
+					"id": 164,
+					"title": "Truck Menu",
+					"description": "",
+					"media_geo_latitude": 40.80832, 
+					"media_geo_longitude": -73.96177,
+					"attributes": {
+							"project_id":719,
+							"frame_id":3187
+					}
+				},
+				{
+					"id": 165,
+					"title": "Korilla BBQ",
+					"description": "",
+					"media_geo_latitude": 40.80832, 
+					"media_geo_longitude": -73.96177,
+					"attributes": {
+							"project_id":645,
+							"frame_id":3691
+					}
+				}
+			],
+			"tags": []
+		}
+	],
+	
+	"request": {}
+}
