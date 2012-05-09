1359d5be6b7ddd920f02c8433aba410f144ea205
Georgia Bullen <georgia.bullen@gmail.com>
2012-05-09 17:48:08 -0400

    adjustments to allison's points

--- js/data/losangeles-allison.js
+++ js/data/losangeles-allison.js
@@ -5,8 +5,8 @@
 			"title": "Los Angeles, CA",
 			"description": "Remittance is often seen as a one way flow. This is not true. Immigrants to this nation bring with them cultural attributes that over time enrich our own. One of the most commonly shared cultural marker is that of cuisine. Mexican immigrants in particular have influenced greatly the food palette of America with the tasty culture of the taco truck. Not only have they made the taco and other dishes mainstays of our culture, but they have brightened streets and imaginations, particularly in LA, with the colorful murals that adorn their trucks. These murals often depict images of their hometown, sharing with passerby's and customers a little history and knowledge along with their tacos.",
 			"text": "",
-			"media_geo_latitude": 24.1, 
-			"media_geo_longitude": -104.6,
+			"media_geo_latitude": 34.070,  
+			"media_geo_longitude": -118.326,
 			"child_items_count": 4,
 			"attributes": {
 				"tiles":"zeega.map-mqd1aurv"
